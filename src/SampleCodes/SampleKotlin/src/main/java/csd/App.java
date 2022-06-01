946bf07fdb18de677eb5f21e2859911cbfa3c809
Oğuz Karan <oguzkaran@csystem.org>
2022-06-01 23:57:25 +0300

    Update

--- src/SampleCodes/SampleKotlin/src/main/java/csd/App.java
+++ src/SampleCodes/SampleKotlin/src/main/java/csd/App.java
@@ -5,23 +5,10 @@
 ----------------------------------------------------------------------------------------------------------------------*/
 package csd;
 
-import java.util.Random;
-import java.util.Scanner;
-
 class App {
     public static void main(String [] args)
     {
-        Random r = new Random();
 
-        int a = r.nextInt(2, 13);
-
-        System.out.printf("a = %d%n", a);
-
-        System.out.println(switch (a) {
-            case 7, 11 -> "Kazandı";
-            case 2, 3, 12 -> "Kaybetti";
-            default -> "Tekrar zarlar atılacak";
-        });
 
     }
 }
