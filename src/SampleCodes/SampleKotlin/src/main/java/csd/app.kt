64ec06a94b74d6d3b8096f20b0010c2da4e4833b
Oğuz Karan <oguzkaran@csystem.org>
2022-06-07 21:04:32 +0300

    Update

--- src/SampleCodes/SampleKotlin/src/main/java/csd/app.kt
+++ src/SampleCodes/SampleKotlin/src/main/java/csd/app.kt
@@ -1,13 +1,30 @@
 /*----------------------------------------------------------------------------------------------------------------------
-    Kotlin'de new operatörü yoktur. Nesne yaratılması aşağıdaki gibi yapılabilir
+    Sınıfın primary ctor'u varsa tüm secondary ctor'ların doğrudan ya da dolaylı olarak bu ctor'u çağırıyor olmaları
+    gerekir. Bu işlem :this ctor sentaksı yapılır
 ----------------------------------------------------------------------------------------------------------------------*/
 package csd
 
 fun main()
 {
-    var d = DeviceInfo()
+    var s = Sample(3)
+    println("------------------------")
+    var k = Sample(3.toShort())
 
     //...
 }
 
-class DeviceInfo
+class Sample(var a: Double) {
+    init {
+        println("primary ctor")
+    }
+
+    constructor(a: Int) : this(a.toDouble())
+    {
+        println("secondary ctor with parameter Int")
+    }
+
+    constructor(a: Short) : this(a.toInt())
+    {
+        println("secondary ctor with parameter Short")
+    }
+}
