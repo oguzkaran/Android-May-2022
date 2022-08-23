4ec2c17d5f261f26b071813be84b4d05cafe6bc2
Oğuz Karan <oguzkaran@csystem.org>
2022-08-23 22:33:40 +0300

    Update

--- src/SampleCodes/SampleKotlin/src/main/java/org/csystem/app/app.kt
+++ src/SampleCodes/SampleKotlin/src/main/java/org/csystem/app/app.kt
@@ -1,47 +1,20 @@
 /*----------------------------------------------------------------------------------------------------------------------
-    apply eklenti fonksiyonu: apply eklenti fonksiyonunun callback'ine this geçirilir
-    "apply the following assignments or calls to the object"
-    apply fonksiyonu çağrıldığı referansa geri döner
+    Aşağıdaki örneği inceleyiniz
 ----------------------------------------------------------------------------------------------------------------------*/
 package org.csystem.app
 
-import org.csystem.kotlin.util.console.readInt
-
-class Sample {
-    var a: Int = 0
-    fun foo()
-    {
-        println("foo")
-    }
-
-    fun bar()
-    {
-        println("bar")
-    }
-
-    fun tar() : Int
-    {
-        println("tar")
-        return  10;
-    }
-
-    override fun toString() = "Sample"
-}
-
 fun main()
 {
-    var x: Int
+    val r = 1..10
+    val iter = r.iterator()
 
-    val s = Sample().apply {
-        a = readInt("Bir sayı giriniz:")
-        this.foo()
-        bar()
-        println("Merhaba")
-        x = tar()
-    }
+    while (iter.hasNext())
+        print("${iter.next()} ")
 
-    println("s.a = ${s.a}")
-    println("x = $x")
-    println(s)
+    println()
+
+    for (v in r)
+        print("$v ")
+
+    println()
 }
-
