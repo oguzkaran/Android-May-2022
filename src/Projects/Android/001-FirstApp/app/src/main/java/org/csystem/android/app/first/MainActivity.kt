58d7d5b7d4fb4e2e9c40dda35c457f75f0da5fe6
Oğuz Karan <oguzkaran@csystem.org>
2022-09-06 21:14:51 +0300

    Update

--- src/Projects/Android/001-FirstApp/app/src/main/java/org/csystem/android/app/first/MainActivity.kt
+++ src/Projects/Android/001-FirstApp/app/src/main/java/org/csystem/android/app/first/MainActivity.kt
@@ -7,15 +7,25 @@ import java.time.LocalDateTime
 import java.time.format.DateTimeFormatter
 
 class MainActivity : AppCompatActivity() {
-    override fun onCreate(savedInstanceState: Bundle?)
-    {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
 
+    private fun displayDateTime()
+    {
         val now = LocalDateTime.now()
 
         val msg = DateTimeFormatter.ofPattern("dd/MM/yyyy kk:mm:ss").format(now)
 
         Toast.makeText(this, msg, Toast.LENGTH_LONG).show()
+    }
+
+    private fun initialize()
+    {
+        displayDateTime()
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?)
+    {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        initialize()
     }
 }
---
+++ src/Projects/Android/002-ActivityLifeCycle/app/src/main/java/org/csystem/android/app/activitylifecycle/MainActivity.kt
@@ -0,0 +1,62 @@
+package org.csystem.android.app.activitylifecycle
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Toast
+
+class MainActivity : AppCompatActivity() {
+    private fun displayCreateReport(savedInstanceState: Bundle?)
+    {
+        Toast.makeText(this, if (savedInstanceState == null) "Created Now" else "Created Before", Toast.LENGTH_LONG).show()
+    }
+
+    private fun initialize(savedInstanceState: Bundle?)
+    {
+        displayCreateReport(savedInstanceState)
+        Toast.makeText(this, "onCreate", Toast.LENGTH_SHORT).show()
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?)
+    {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        initialize(savedInstanceState)
+    }
+
+    override fun onStart()
+    {
+        super.onStart()
+        Toast.makeText(this, "onStart", Toast.LENGTH_SHORT).show()
+    }
+
+    override fun onRestart()
+    {
+        super.onRestart()
+        Toast.makeText(this, "onRestart", Toast.LENGTH_SHORT).show()
+    }
+
+    override fun onResume()
+    {
+        super.onResume()
+        Toast.makeText(this, "onResume", Toast.LENGTH_SHORT).show()
+    }
+
+    override fun onPause()
+    {
+        super.onPause()
+        Toast.makeText(this, "onPause", Toast.LENGTH_SHORT).show()
+    }
+
+    override fun onStop()
+    {
+        super.onStop()
+        Toast.makeText(this, "onStop", Toast.LENGTH_SHORT).show()
+    }
+
+    override fun onDestroy()
+    {
+        super.onDestroy()
+        Toast.makeText(this, "onDestroy", Toast.LENGTH_SHORT).show()
+    }
+}
