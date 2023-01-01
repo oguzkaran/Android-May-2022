03c6aecee8643a9f445e630a0b07b4ce240428b5
Oğuz Karan <oguzkaran@csystem.org>
2023-01-01 13:51:59 +0300

    Update

--- src/Projects/Android/014-SampleHiltApplication/diconfiglib/src/main/java/org/csystem/android/app/hiltdi/configuration/inject/annotation/mapper/TimeStringMapperAuthInterceptor.kt
+++
@@ -1,7 +0,0 @@
-package org.csystem.android.app.hiltdi.configuration.inject.annotattion.mapper
-
-import javax.inject.Qualifier
-
-@Qualifier
-@Retention(AnnotationRetention.BINARY)
-annotation class TimeStringMapperAuthInterceptor
--- src/Projects/Android/014-SampleHiltApplication/hiltdi/src/main/java/org/csystem/android/app/hiltdi/data/viewmodel/MainActivityViewModel.kt
+++
@@ -1,8 +0,0 @@
-package org.csystem.android.app.hiltdi.data.viewmodel
-
-import org.csystem.android.app.hiltdi.MainActivity
-
-data class MainActivityViewModel(val activity: MainActivity) {
-    fun handleFirstButton() = activity.firstButtonClicked()
-    fun handleSecondtButton() = activity.secondButtonClicked()
-}
