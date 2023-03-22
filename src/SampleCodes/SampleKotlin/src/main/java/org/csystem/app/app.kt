0747e68da4c46877719d3644c3b88e97bb3ea05a
Oğuz Karan <oguzkaran@csystem.org>
2023-03-23 01:03:18 +0300

    Update

--- src/SampleCodes/SampleKotlin/src/main/java/org/csystem/app/app.kt
+++ src/SampleCodes/SampleKotlin/src/main/java/org/csystem/app/app.kt
@@ -1,22 +1,38 @@
 /*----------------------------------------------------------------------------------------------------------------------
-
+    Aşağıdaki örneği inceleyiniz
 ----------------------------------------------------------------------------------------------------------------------*/
 package org.csystem.app
 
-val list = ArrayList<Sample>()
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.runBlocking
 
 fun main()
 {
+    val f = suspend {
+        val job = GlobalScope.launch {
+            for (i in 1..10) {
+                print("$i ")
+                delay(1000)
+            }
+            println()
+        }
 
+        job.join()
+
+        print("end!...")
+        //...
+    }
+
+    doWork(f)
+
+    //...
 }
 
-
-fun doWork(value: Int)
+fun doWork(block: suspend () -> Unit)
 {
-
-}
-
-
-class Sample {
-
+    //...
+    runBlocking { block() }
+    //...
 }
